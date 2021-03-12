bc5a5f0e31dca0895b917d8c5447ee1357ed18a0
89769 <karan456371@gmail.com>
2021-03-12 12:20:59 +0530

    converter

---
+++ app/src/main/java/com/example/conveter/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.conveter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void btnclick(View view){
+        EditText dollarAmount=(EditText) findViewById(R.id.dollarAmount);
+        String dollars = dollarAmount.getText().toString();
+        Double doubleDollars = Double.parseDouble(dollars);
+        Double doubleInr = 71.72 * doubleDollars;
+        String toastText = "=" + doubleInr.toString() + "$";
+
+        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
